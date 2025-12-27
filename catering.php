ce80d6ff075b89363fe9d1536c8219d6a4fca835
Prasanna_Surisetty <prasannasurisetty9@gmail.com>
2025-12-27 14:35:39 +0530

    27-12.1

--- catering.php
+++ catering.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+    <title>Catering</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
